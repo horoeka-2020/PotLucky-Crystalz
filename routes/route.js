f1bd8417457aef9df70bdc65106f79d09500ee49
Caz <cjpitcher@gmail.com>
2020-11-13 13:46:05 +1300

    Changed route address

--- routes/route.js
+++ routes/route.js
@@ -4,7 +4,7 @@ const router = express.Router()
 
 const data = require('../data/recipes')
 
-router.get('/recipes/:id', (req, res) => {
+router.get('/recipes', (req, res) => {
   req.then((recipes) => {
       return res.status(200).json(recipes)
     })
